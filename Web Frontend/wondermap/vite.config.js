7bbeb88ef54deb5376d0644ff3437a35e5e93402
nethmiTK <nethmitk33@gmail.com>
2025-03-31 14:10:38 +0530

    vite download

--- Web Frontend/wondermap/vite.config.js
+++ Web Frontend/wondermap/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path'; // This line is crucial
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
